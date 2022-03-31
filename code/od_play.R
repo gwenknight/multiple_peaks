0df2196c2419ecc5a8483e80a1ca8e7fb6684ab9
gwenknight <gwen.knight@github.com>
2022-03-31 08:37:02 +0100

    trying to run for all inoc

--- code/od_play.R
+++ code/od_play.R
@@ -3,15 +3,21 @@
 library(zoo)
 library(imputeTS) # to give weighter moving average - exponential weighting of those further away
 library(patchwork)
+library(tidyverse)
 setwd(here::here())
 
 # Data to explore
 data_od_orig <- read_csv("data/growth_ODvsCS_20220224.csv")[,-1]
 
+
 ## Look at OD & CS data
 ggplot(data_od_orig, aes(x=Time, y = value, group = interaction(inoc, exp, strain, rep))) + geom_line(aes(col = factor(inoc))) + 
   facet_grid(exp~strain, scales = "free")
 ggsave("plots/ODvsC2_rawdata.pdf")
+
+#######################********** 10^5 ****************##############################################################################################################################################################
+#### JUST DO FOR 10^5 to start! 
+############################################################################################################################################################################################################################
 
 ggplot(data_od_orig %>% filter(inoc == 5), aes(x=Time, y = value, group = interaction(inoc, exp, strain, rep))) + geom_line(aes(col = factor(rep))) + 
   facet_grid(exp~strain, scales = "free")
@@ -24,8 +30,6 @@ data_od <- data_od_orig %>% dplyr::select(Time, rep, exp, value, strain, inoc) %
          differ = c(0,diff(ma_value)),
          compara = ifelse(exp == "CS", value, differ)) %>%
   ungroup() 
-
-
 
 g2 <- ggplot(data_od, aes(x=Time, y = ma_value, group = interaction(rep, exp, strain))) + 
   geom_line(aes(col = factor(rep))) + 
@@ -80,7 +84,8 @@ g1 <- ggplot(data_od_normd_ana, aes(x=Time, y = nongrowth_only, group = interact
   facet_wrap(~strain, ncol = 4) + 
   scale_x_continuous("Time (h)") + 
   scale_y_continuous("Non growth only") + 
-  scale_color_discrete("Replicate")
+  scale_color_discrete("Replicate") + 
+  geom_hline(yintercept = 0)
 
 g2 <- ggplot(data_od_normd_ana, aes(x=Time, group = interaction(exp,rep, strain))) + 
   geom_line(aes(y = imput_val,col = interaction(rep), linetype = exp)) + 
@@ -167,6 +172,191 @@ dim(param)
 
 ## Store so don't have to run above
 write.csv(param, "output/param_od.csv")
+#param <- read_csv("output/param_od.csv")[,-1]
+
+### look at OD vs CS
+param_wide_timepeak <- param %>% pivot_wider(id_cols = c(strain, rep),names_from = experiment, values_from = c(timepeak))
+ggplot(param, aes(x=interaction(strain,rep), y = timepeak)) + geom_point(aes(col = experiment))
+
+ggplot(param_wide_timepeak, aes(x=CS, y = OD)) + geom_point(aes(col = strain)) + 
+  scale_y_continuous(lim = c(0,31000)) + 
+  geom_smooth(method='lm', formula= y~x)
+
+ggplot(param, aes(x=interaction(strain,rep), y = exp_gr)) + geom_point(aes(col = experiment))
+
+### clustering? 
+data_od$drytime <- 0
+param$drytime <- 0
+data_od$inoc <- 5
+param$inoc <- 5
+data_od$strain <- as.character(data_od$strain)
+param$strain <- as.character(param$strain)
+data_od$value_J <- data_od$compara
+
+
+c_cs <- cluster(data_od %>% filter(exp == "CS"), param %>% filter(experiment == "CS"),plot_where = "plots/CS_")
+c_od <- cluster(data_od %>% filter(exp == "OD"), param %>% filter(experiment == "OD"),plot_where = "plots/OD_")
+
+
+#######################********** ALL ****************##############################################################################################################################################################
+#### All inocula
+############################################################################################################################################################################################################################
+
+ggplot(data_od_orig, aes(x=Time, y = value, group = interaction(inoc, exp, strain, rep))) + geom_line(aes(col = factor(rep))) + 
+  facet_grid(exp + inoc~strain, scales = "free")
+ggsave("plots/ODvsC2_rawdata.pdf")
+
+# remove all data before the time point that they all have which is the max of the minimum times to avoid odd completion curves prior to start and after
+cutoff_time_dn = max(data_od_orig %>% dplyr::group_by(rep, exp, inoc) %>% dplyr::summarise(minn = min(Time)) %>% ungroup() %>% dplyr::select(minn))
+cutoff_time_up = min(data_od_orig %>% dplyr::group_by(rep, exp, inoc) %>% dplyr::summarise(maxx = max(Time)) %>% ungroup() %>% dplyr::select(maxx))
+
+data_od <- data_od_orig %>% filter(Time > cutoff_time_dn, Time < cutoff_time_up) %>% 
+  dplyr::select(Time, rep, exp, value, strain, inoc) %>% 
+  dplyr::group_by(rep, exp, strain, inoc) %>%
+  dplyr::mutate(group = interaction(rep, exp, strain,inoc))%>%
+  dplyr::group_by(group) %>%
+  dplyr::mutate(ma_value = rollapply(value, 10, mean,fill = NA,align = "right", partial = TRUE), #### NOT RESPECTING GROUPS??
+                differ = c(0,diff(ma_value)),
+                compara = ifelse(exp == "CS", value, differ)) %>%
+  dplyr::ungroup() %>% filter(Time > cutoff_time_dn, Time < cutoff_time_up)
+
+g2 <- ggplot(data_od, aes(x=Time, y = ma_value, group = interaction(rep, exp, strain))) + 
+  geom_line(aes(col = factor(rep))) + 
+  facet_grid(exp + inoc~strain,scales = "free") + 
+  scale_x_continuous("Time (h)") 
+ggsave("plots/ODvsCS_data_smoothed.pdf")
+
+g3 <- ggplot(data_od, aes(x=Time, y = compara, group = interaction(rep, exp, strain))) + 
+  geom_line(aes(col = factor(rep),lty = factor(exp))) + 
+  facet_grid(inoc + exp~strain, scales = "free") + 
+  scale_x_continuous("Time (h)") + scale_color_discrete("Replicate") + scale_linetype_discrete("Data source")
+ggsave("plots/ODvsCS_data_compare.pdf")
+
+# Try to plot together? scale by max. Normalise
+data_od <- data_od %>% dplyr::group_by(inoc, strain, rep, exp) %>% dplyr::mutate(max_v = max(compara, na.rm = TRUE),
+                                                                                 compara_norm = compara / max_v)
+
+ggplot(data_od, aes(x=Time, y = compara_norm, group = interaction(rep, exp, strain))) + 
+  geom_line(aes(col = interaction(exp, rep), lty = exp), lwd = 1) + 
+  facet_grid(inoc ~strain, scales = "free") + 
+  scale_x_continuous("Time (h)") 
+ggsave("plots/ODvsCS_data_compare_norm.pdf")
+
+# Subtract normalised data? Need to complete: measured at different time points
+
+data_od_normd <- data_od %>% ungroup() %>% filter(Time > cutoff_time_dn, Time < cutoff_time_up) %>% # make sure cover same time for all 
+  complete(rep, strain, exp, inoc, Time) %>% dplyr::mutate(compara_norm_inp = na_ma(compara_norm, k = 4, weighting = "linear", maxgap = 10)) %>% # fill in all time points and then linear imputation between (tried exponential and simple but get more odd bumps)
+  dplyr::group_by(inoc, strain, rep, exp) %>% dplyr::select(Time, strain,rep, exp, inoc, compara_norm_inp) %>% # Take imputed values
+  pivot_wider(id_cols = c(strain, Time, rep, inoc), names_from = exp, values_from = compara_norm_inp) %>%dplyr::mutate(nongrowth_only = CS - OD) %>% # look for difference between OD and heat output
+  pivot_longer(cols = c("CS","OD"), names_to = "exp", values_to ="imput_val")
+
+data_od_normd_ana <- left_join(data_od, data_od_normd)
+
+ggplot(data_od_normd_ana, aes(x=Time, y = nongrowth_only, group = interaction(exp,rep, strain))) + 
+  geom_line(aes(col = interaction(exp,rep)), lwd = 1) + 
+  geom_line(aes(y = imput_val,col = interaction(exp,rep))) + 
+  facet_grid(inoc~strain, scales = "free") + 
+  scale_x_continuous("Time (h)") 
+ggsave("plots/ODvsCS_data_nongrowth_togplot.pdf")
+
+
+g1 <- ggplot(data_od_normd_ana, aes(x=Time, y = nongrowth_only, group = interaction(rep, strain))) + 
+  geom_line(aes(col = interaction(rep))) + 
+  facet_grid(inoc~strain) + 
+  scale_x_continuous("Time (h)") + 
+  scale_y_continuous("Non growth only") + 
+  scale_color_discrete("Replicate") + 
+  geom_hline(yintercept = 0)
+
+g2 <- ggplot(data_od_normd_ana, aes(x=Time, group = interaction(exp,rep, strain))) + 
+  geom_line(aes(y = imput_val,col = interaction(rep), linetype = exp)) + 
+  facet_grid(inoc~strain, scales = "free") + 
+  scale_x_continuous("Time (h)") + 
+  scale_y_continuous("Normalised measure") + 
+  scale_color_discrete("Experiment and\nreplicate")
+
+g2 / g1 
+ggsave("plots/ODvsCS_data_nongrowth_tog_grid.pdf")
+
+
+### Extract characteristics
+
+## What are the strains?
+u <- as.character(unique(data_od$strain,stringsasFactors = FALSE)) # strains
+## How many replicates? 
+r <- unique(data_od$rep) # replicates
+## How many experimental conditions? 
+ex <- unique(data_od$exp)
+## How many inoc?
+inn <- unique(data_od$inoc)
+
+# Where the parameters for each strain are stored
+param <- matrix(0, length(u)*length(r)*length(ex)*length(inn), 51); # number of strains x number of replicates x number of experimental conditions
+index <- 1 # for counting 
+max <- c() # for calibration
+
+data_od <- data_od %>% dplyr::ungroup()
+
+## Remove odd one that doesn't peak.. 
+w <- intersect(which(data_od$strain == 11051), which(data_od$inoc == 1))
+data_od <- data_od[-w,]
+
+## Run thru each strain/rep/drying time/ inoculum: fit a growth curve to each
+for(jj in 1:length(u)){ # for each strain
+  r <- unlist(unique(data_od %>% filter(strain == u[jj]) %>% dplyr::select(rep))[,1]) # which replicates for this strain (all have different names)
+  if(length(r) < 3){print(paste0(u[jj], " has too few replicates"))}else{ # filter out if only 2 replicates
+    for(ii in 1:length(r)){ # for each replicate
+      for(pp in 1:length(inn)){ # for each inoc
+        for(kk in 1:length(ex)){ #each of the experimental conditions
+          
+          data1 <- data_od %>% filter(strain == u[jj], rep == r[ii], inoc == inn[pp], exp == ex[kk])
+          
+          if(dim(data1)[1] > 0){ # if this replicate exists for this strain (i.e. there is data)
+            
+            if(length(which(is.na(data1$compara)))>0){data1 <- data1[-which(is.na(data1$compara)),]} # remove any NA
+            
+            print(c(jj, u[jj], r[ii], inn[pp],ex[kk])) # output so can track how it is working
+            p <- cut_extract_dp(data1, "Time", "compara", paste(strain, replicate, condition, inocl,sep="_")) ### NEW function: runs on any timeseries: gives baseline and cut parameter analysis
+            
+            ## Required parameters
+            
+            param[index,] <- c(u[jj], r[ii], inn[pp],ex[kk], unlist(p$param))
+            index <- index + 1 # counting for storing matrix - next row each iteration
+          }
+          
+        }
+      }
+    }
+  }
+}
+
+# The original set 
+param_orig <- param
+
+## Fitted parameters
+param <- as.data.frame(param)
+colnames(param) <- c("strain","rep","inoc","experiment",
+                     "t_m_h_flow", "v_m_h_flow", "exp_gr","lag","auc",
+                     "odd_peaks","odd_width","width_peak","odd_shoulder","odd_shoulder_past","odd_double",
+                     "shoulder_point_t","shoulder_point_v", "shoulder_point_past_t","shoulder_point_past_v",
+                     "cut_exp", "timepeak", "valpeak",
+                     "mp_t1","mp_t2","mp_t3","mp_t4","mp_t5","mp_t6","mp_t7","mp_t8","mp_t9","mp_t10",
+                     "mp_h1","mp_h2","mp_h3","mp_h4","mp_h5","mp_h6","mp_h7","mp_h8","mp_h9","mp_h10",
+                     "gap1","gap2","gap3","gap4","gap5","gap6","gap7","gap8","gap9")
+
+w<-which(param$lag!=0); param <- param[w,] # remove 0 values
+param$rep <- as.numeric(param$rep)
+param$odd_peaks <- as.numeric(param$odd_peaks)
+param$odd_width <- as.numeric(param$odd_width)
+param$odd_shoulder <- as.numeric(param$odd_shoulder)
+param$odd_shoulder_past <- as.numeric(param$odd_shoulder_past)
+param$auc <- as.numeric(param$auc)
+param$valpeak <- as.numeric(param$valpeak)
+param$timepeak <- as.numeric(param$timepeak)
+dim(param)
+
+## Store so don't have to run above
+write.csv(param, "output/param_od_allinoc.csv")
 #param <- read_csv("output/param_od.csv")[,-1]
 
 ### look at OD vs CS
