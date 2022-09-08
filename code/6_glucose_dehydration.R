2520b6264ee0dd8ed78beda5546173b16b5e6685
gwenknight <gwen.knight@lshtm.ac.uk>
2022-09-08 21:03:16 +0100

    ddplyr::summarise needed

--- code/6_glucose_dehydration.R
+++ code/6_glucose_dehydration.R
@@ -27,7 +27,13 @@ setwd(here::here())
 # 3 reps  7 inoc / 4 gluc
 data1 <- read_csv("data/glucose_HF_noBC_t8.csv")[,-1] # with baseline correction 
 data2 <- read_csv("data/glucose_HF_t8.csv")[,-1] # without baseline correction 
-data0 <- read_csv("data/glucose_HF.csv") # time 0 data 
+data01 <- read_csv("data/glucose_HF.csv")[,-1] # time 0 data 
+data02 <- read_csv("data/glucose_HF_noBC.csv")[,-1] # time 0 data without baseline correction
+
+data01$drytime <- 0
+data02$drytime <- 0
+data1$drytime <- 7
+data2$drytime <- 7
 
 data_dry <- data1 # with correction (automatic from calscreener)
 data_dry$value_base <- data2$value # (without correction)
@@ -51,46 +57,49 @@ ggsave("plots/glucose_drying_raw_data_base_correct.png")
 #######################********** Extract characteristics ****************##############################################################################################################################################################
 #### Extract key parameters from the HF data after 7 days drying (NO OD data so can't get non-growth)
 ############################################################################################################################################################################################################################
-data_hf_t8 <- rbind(data1, data2) # on top of each other instead of alongside
+data_hf <- rbind(data1, data2, data01, data02) # on top of each other instead of alongside
 
 ## What are the glucose levels?
-u <- as.character(unique(data_hf_t8$glucose,stringsasFactors = FALSE)) # strains
+u <- as.character(unique(data_hf$glucose,stringsasFactors = FALSE)) # strains
 ## How many replicates? 
-r <- unique(data_hf_t8$rep) # replicates
+r <- unique(data_hf$rep) # replicates
 ## How many inoc?
-inn <- unique(data_hf_t8$inoc)
+inn <- unique(data_hf$inoc)
 ## Baseline correction yes / no
-bc <- unique(data_hf_t8$baseline)
+bc <- unique(data_hf$baseline)
+## drytimes
+dry <- unique(data_hf$drytime)
 
 # Where the parameters for each strain are stored
-param <- matrix(0, length(u)*length(r)*length(inn)*length(bc), 10); # number of strains x number of replicates x number of experimental conditions
+param <- matrix(0, length(u)*length(r)*length(inn)*length(bc)*length(dry), 11); # number of strains x number of replicates x number of experimental conditions
 index <- 1 # for counting 
 max <- c() # for calibration
 
-data_hf_t8 <- data_hf_t8 %>% dplyr::ungroup()
+data_hf <- data_hf %>% dplyr::ungroup()
 
 ## Run thru each glucose/rep/drying time/ inoculum: fit a growth curve to each
 for(jj in 1:length(u)){ # for each strain
-  r <- unlist(unique(data_hf_t8 %>% filter(glucose == u[jj]) %>% dplyr::select(rep))[,1]) # which replicates for this glucose (all have different names)
+  r <- unlist(unique(data_hf %>% filter(glucose == u[jj]) %>% dplyr::select(rep))[,1]) # which replicates for this glucose (all have different names)
   if(length(r) < 3){print(paste0(u[jj], " has too few replicates"))}else{ # filter out if only 2 replicates
     for(ii in 1:length(r)){ # for each replicate
       for(pp in 1:length(inn)){ # for each inoc
         for(qq in 1:length(bc)){ # for each baseline
-          
-          data1 <- data_hf_t8 %>% filter(glucose == u[jj], rep == r[ii], inoc == inn[pp], baseline == bc[qq])
-          data2 <- data1 
-          data2$value <- 0 # set to zero so AUC = 0 => subtraction gives data 1 AUC in below
-          
-          if(dim(data1)[1] > 0){ # if this replicate exists for this glucose (i.e. there is data)
+          for(dp in 1:length(dry)){ # for each drytime
             
-            print(c(jj, u[jj], r[ii], inn[pp],bc[qq])) # output so can track how it is working
-            p <- charac_extract(data1, "Time", "value", data1, data2, "value", paste(u[jj], r[ii], inn[pp], bc[qq],sep="_")) ### NEW function: simplification of more complex data recognition for clustering
+            data1 <- data_hf %>% filter(glucose == u[jj], rep == r[ii], inoc == inn[pp], baseline == bc[qq], drytime == dry[dp])
+            data2 <- data1 
+            data2$value <- 0 # set to zero so AUC = 0 => subtraction gives data 1 AUC in below
             
-            ## Required parameters
-            param[index,] <- c(u[jj], r[ii], inn[pp],bc[qq], p)
-            index <- index + 1 # counting for storing matrix - next row each iteration
+            if(dim(data1)[1] > 0){ # if this replicate exists for this glucose (i.e. there is data)
+              
+              print(c(jj, u[jj], r[ii], inn[pp],bc[qq],dry[dp])) # output so can track how it is working
+              p <- charac_extract(data1, "Time", "value", data1, data2, "value", paste(u[jj], r[ii], inn[pp], bc[qq],dry[dp],sep="_")) ### NEW function: simplification of more complex data recognition for clustering
+              
+              ## Required parameters
+              param[index,] <- c(u[jj], r[ii], inn[pp],bc[qq],dry[dp], p)
+              index <- index + 1 # counting for storing matrix - next row each iteration
+            }
           }
-          
         }
       }
     }
@@ -103,7 +112,7 @@ param_orig <- param
 ## Fitted parameters
 #param <- param[-which(param[,3]==0),] # remove any empty rows (when replicates < 3)
 param <- as.data.frame(param)
-colnames(param) <- c("glucose","rep","inoc","baseline",
+colnames(param) <- c("glucose","rep","inoc","baseline","drytime",
                      "t_max_h_flow", "v_max_h_flow", 
                      "t_min_h_flow", "v_min_h_flow", 
                      "auc","lagtime")
@@ -112,72 +121,73 @@ param$auc <- as.numeric(param$auc)
 dim(param)
 
 ## Store so don't have to run above
-write.csv(param, "output/simple_extract_glucose_allinoc_t8hf.csv")
-param <- read_csv("output/simple_extract_glucose_allinoc_t8hf.csv")[,-1]
+write.csv(param, "output/simple_extract_glucose_allinoc_hf.csv")
+param <- read_csv("output/simple_extract_glucose_allinoc_hf.csv")[,-1]
 
 ##########################################################################################
 #### *********** Analyse above characteristics*************** ####################
 ##########################################################################################
 ## Take mean over replicates
-param_mean_bothbl <- param %>% group_by(glucose, inoc, baseline) %>% summarise(mean_t_max = mean(t_max_h_flow),mean_h_max = mean(v_max_h_flow),
-                                                                               mean_t_min = mean(t_min_h_flow),mean_h_min = mean(v_min_h_flow),
-                                                                               mean_auc = mean(auc))
+param_mean_bothbl <- param %>% group_by(glucose, inoc, baseline, drytime) %>% dplyr::summarise(mean_t_max = mean(t_max_h_flow),mean_h_max = mean(v_max_h_flow),
+                                                                                        mean_t_min = mean(t_min_h_flow),mean_h_min = mean(v_min_h_flow),
+                                                                                        mean_auc = mean(auc))
 
 #### Two baselines
 for(i in c("yes", "no")){
-  param_mean = param_mean_bothbl %>% filter(baseline == i)
-  
-  # Heatmaps over inocula
-  g1 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_t_max)) + geom_tile() +
-    scale_fill_distiller(palette = "RdBu","Time to\nmax", ) + 
-    scale_x_continuous("Inoculum") + 
-    scale_y_discrete("Glucose concentration") #+ 
-  #ggtitle("Time to max non-growth output")
-  
-  g2 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_h_max)) + geom_tile() +
-    scale_fill_distiller(palette = "RdBu","Height of\nmax", ) + 
-    scale_x_continuous("Inoculum") + 
-    scale_y_discrete("Glucose concentration") #+ 
-  #ggtitle("Height of max non-growth output")
-  
-  g3 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_t_min)) + geom_tile() +
-    scale_fill_distiller(palette = "RdBu","Time to\nmin", ) + 
-    scale_x_continuous("Inoculum") + 
-    scale_y_discrete("Glucose concentration") #+ 
-  #ggtitle("Time to min non-growth output")
-  
-  g4 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_h_min)) + geom_tile() +
-    scale_fill_distiller(palette = "RdBu","Height of \nmin", ) + 
-    scale_x_continuous("Inoculum") + 
-    scale_y_discrete("Glucose concentration") #+ 
-  #ggtitle("Height of min non-growth output")
-  
-  g5 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_auc)) + geom_tile() +
-    scale_fill_distiller(palette = "RdBu","AUC ", ) + 
-    scale_x_continuous("Inoculum") + 
-    scale_y_discrete("Glucose concentration") #+ 
-  #ggtitle("AUC non-growth output")
-  
-  g1 + g3 + g5 + g2 + g4 + plot_layout(ncol = 3) + plot_annotation(title = paste0("Baseline correction: ", i))
-  ggsave(paste0("plots/glucose_summary_heatmap_t8hf_",i,".png"), width = 12, height = 8)
-  
-  #### Plot over glucose concentrations
-  param_long <- param %>% pivot_longer(cols = t_max_h_flow:auc) %>% filter(baseline == i)
-  param_long$name <- factor(param_long$name, levels = c("t_max_h_flow","t_min_h_flow" ,"auc", "v_max_h_flow","v_min_h_flow"))
-  
-  ggplot(param_long, aes(x=inoc, y = value, group = interaction(glucose,rep))) + geom_line(aes(col = factor(glucose))) + 
-    facet_wrap(~name, scales = "free") + 
-    scale_color_discrete("Glucose concentration") + 
-    scale_x_continuous("Inoculum") + 
-    ggtitle(paste0("Baseline correction: ", i))
-  ggsave(paste0("plots/glucose_summary_lines_t8hf_",i,".png"))
-  
-  ggplot(param_long, aes(x=inoc, y = value, group = interaction(glucose))) + geom_point(aes(col = factor(glucose))) + 
-    geom_smooth(aes(col = factor(glucose), fill = factor(glucose)),method='lm', formula= y~x) + 
-    facet_wrap(~name, scales = "free") + 
-    scale_color_discrete("Glucose concentration") + scale_fill_discrete("Glucose concentration") + 
-    scale_x_continuous("Inoculum") + 
-    ggtitle(paste0("Baseline correction: ", i))
-  ggsave(paste0("plots/glucose_summary_smoothed_t8hf_",i,".png"))
-  
+  for(j in c(0,7)){
+    param_mean = param_mean_bothbl %>% filter(baseline == i, drytime == j)
+    
+    # Heatmaps over inocula
+    g1 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_t_max)) + geom_tile() +
+      scale_fill_distiller(palette = "RdBu","Time to\nmax", ) + 
+      scale_x_continuous("Inoculum") + 
+      scale_y_discrete("Glucose concentration") #+ 
+    #ggtitle("Time to max non-growth output")
+    
+    g2 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_h_max)) + geom_tile() +
+      scale_fill_distiller(palette = "RdBu","Height of\nmax", ) + 
+      scale_x_continuous("Inoculum") + 
+      scale_y_discrete("Glucose concentration") #+ 
+    #ggtitle("Height of max non-growth output")
+    
+    g3 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_t_min)) + geom_tile() +
+      scale_fill_distiller(palette = "RdBu","Time to\nmin", ) + 
+      scale_x_continuous("Inoculum") + 
+      scale_y_discrete("Glucose concentration") #+ 
+    #ggtitle("Time to min non-growth output")
+    
+    g4 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_h_min)) + geom_tile() +
+      scale_fill_distiller(palette = "RdBu","Height of \nmin", ) + 
+      scale_x_continuous("Inoculum") + 
+      scale_y_discrete("Glucose concentration") #+ 
+    #ggtitle("Height of min non-growth output")
+    
+    g5 <- ggplot(param_mean, aes(inoc, factor(glucose), fill= mean_auc)) + geom_tile() +
+      scale_fill_distiller(palette = "RdBu","AUC ", ) + 
+      scale_x_continuous("Inoculum") + 
+      scale_y_discrete("Glucose concentration") #+ 
+    #ggtitle("AUC non-growth output")
+    
+    g1 + g5 + g2 + plot_layout(ncol = 1) + plot_annotation(title = paste0("Baseline correction: ", i, " Drytime: ",j))
+    ggsave(paste0("plots/glucose_summary_heatmap_hf_",i,j,".png"), width = 5, height = 8)
+    
+    #### Plot over glucose concentrations
+    param_long <- param %>% pivot_longer(cols = t_max_h_flow:auc) %>% filter(baseline == i) %>% filter(name %in% c("t_max_h_flow" ,"auc", "v_max_h_flow"))
+    param_long$name <- factor(param_long$name, levels = c("t_max_h_flow" ,"auc", "v_max_h_flow"))
+    
+    ggplot(param_long, aes(x=inoc, y = value, group = interaction(glucose,rep))) + geom_line(aes(col = factor(glucose))) + 
+      facet_wrap(~name, scales = "free") + 
+      scale_color_discrete("Glucose concentration") + 
+      scale_x_continuous("Inoculum") + 
+      ggtitle(paste0("Baseline correction: ", i, " Drytime: ",j))
+    ggsave(paste0("plots/glucose_summary_lines_hf_",i,j,".png"))
+    
+    ggplot(param_long, aes(x=inoc, y = value, group = interaction(glucose))) + geom_point(aes(col = factor(glucose))) + 
+      geom_smooth(aes(col = factor(glucose), fill = factor(glucose)),method='lm', formula= y~x) + 
+      facet_wrap(~name, scales = "free") + 
+      scale_color_discrete("Glucose concentration") + scale_fill_discrete("Glucose concentration") + 
+      scale_x_continuous("Inoculum") + 
+      ggtitle(paste0("Baseline correction: ", i, " Drytime: ",j))
+    ggsave(paste0("plots/glucose_summary_smoothed_hf_",i,j,".png"))
+  }
 }
