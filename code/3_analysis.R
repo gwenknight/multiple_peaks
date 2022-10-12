fd2e86ce2c90d289d9d38f9708db1f1f1ae4b524
Valérie Baede <vbaede@gmail.com>
2022-10-12 13:57:50 +0200

    Adjust figure 3

--- code/3_analysis.R
+++ code/3_analysis.R
@@ -49,6 +49,23 @@ g <- ggplot(cluster_data %>% filter(drytime ==0, inocl == 5) %>% filter(name %in
   geom_boxplot(aes(fill = cluster)) +  facet_wrap(~name,ncol = 4, scales = "free") 
 ggsave("plots/inoc_5_story_boxplot.pdf", width = 20, height = 8)
 
+# var_name <- c(
+#   auc = "AUC",
+#   valpeak = "Max value 1st peak",
+#   exp_gr = "Max exp growth rate",
+#   second_peak_h = "Max value 2nd peak"
+# )
+
+# ggplot(cluster_data %>% filter(drytime ==0, inocl == 5) %>% filter(name %in% c("auc", "valpeak","exp_gr","second_peak_h")) %>% 
+#          mutate(across(name, factor, levels=c("auc", "valpeak","exp_gr","second_peak_h"))), aes(x = cluster, y = value)) + 
+#   geom_boxplot(aes(fill = cluster), show.legend = FALSE) +  
+#   scale_x_discrete("Cluster type", label = c("Normal","Double","Spike","Post-shoulder","Wide","Unclustered")) +
+#   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+#   scale_y_continuous("Value") +
+#   facet_wrap(~name, ncol = 4, scales = "free", labeller = labeller(name = var_name)) +
+#   labs(fill = "Cluster")
+# ggsave("plots/final/figure3.png", width = 10, height = 5)
+
 g + stat_compare_means(comparisons = my_comparisons)
 ggsave("plots/inoc_5_story_boxplot_stats.pdf", width = 20, height = 8)
 
