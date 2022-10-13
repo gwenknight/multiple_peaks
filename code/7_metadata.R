8002796cb7bfa0459371d08ac595207415a1d497
Valérie Baede <vbaede@gmail.com>
2022-10-13 18:00:45 +0200

    Updated statistics

--- code/7_metadata.R
+++ code/7_metadata.R
@@ -17,6 +17,8 @@ library(patchwork)
 library(ggpubr)
 library(viridis)
 library(gridExtra)
+library(mlogit)
+library(gmodels)
 
 theme_set(theme_bw(base_size=12)) # theme setting for plots: black and white (bw) and font size (24)
 mycolors <- colorRampPalette(brewer.pal(8, "Set2"))(14)
@@ -38,7 +40,7 @@ c$parameters <- read_csv("output/clustered_parameters.csv")
 # Then deduplicate to keep that one cluster type per strain
 
 mdd_cluster <- c$parameters %>% 
-  dplyr::select(inocl, cluster, strain, drytime, rep) %>% 
+  dplyr::select(inocl, cluster, strain, drytime, rep, width_peak) %>% #or without width_peak
   filter(drytime == 0) %>%
   filter(inocl == 5) %>% 
   filter(!(strain %in% c("Newman", "RWW12", "SA3297", "SA2704", "RWW146", "SAC042W", "Mu50", "M116")))
@@ -72,16 +74,16 @@ mdd <- as.data.frame(mdd)
 mdd_cluster <- as.data.frame(mdd_cluster)
 
 mdd$strain <- as.character(mdd$strain)
-anti_join(mdd, mdd_cluster) #data for strain 11287 present in mdd, but not in mdd_cluster
-arrange(mdd_cluster, strain) #indeed, strain 11287 not present in mdd_cluster
+#anti_join(mdd, mdd_cluster) #data for strain 11287 present in mdd, but not in mdd_cluster
+#arrange(mdd_cluster, strain) #indeed, strain 11287 not present in mdd_cluster
 
 mdd <- mdd %>%
   filter(!(strain %in% c("11287")))
 
-length(unique(mdd$strain))
-length(unique(mdd_cluster$strain))
-dim(mdd) # 97 118
-dim(mdd_cluster) #97  5
+# length(unique(mdd$strain))
+# length(unique(mdd_cluster$strain))
+# dim(mdd) # 97 118
+# dim(mdd_cluster) #97  5
 
 mdd_all <- merge(mdd_cluster, mdd, by.x = "strain")
 dim(mdd_all)
@@ -95,13 +97,145 @@ dim(mdd_amr) # 97 37
 mdd_vir <- dplyr::select(mdd_all, strain, cluster, original_ID : spa_type, adsA : vWbp) #metadata + virulence gene data
 dim(mdd_vir) # 97 99
 #names(mdd_vir)
-mdd1 <- dplyr::select(mdd_all, strain, cluster, original_ID : spa_type) #metadata
+#mdd1 <- dplyr::select(mdd_all, strain, cluster, original_ID : spa_type) #metadata
+mdd1 <- dplyr::select(mdd_all, strain, cluster, width_peak : spa_type) #metadata
 dim(mdd1) # 97 17
-#names(mdd1)
+#names(mdd1) #cluster, country, year, success, carriage, site, lineage, mec, pvl, spa_type #width_peak
+
+## Data description ####
+
+mdd1%>%
+  count(cluster)
+
+# cluster  n
+# 1        double  5
+# 2        normal 56
+# 3 post_shoulder 20
+# 4         spike  6
+# 5   unclustered  2
+# 6          wide  8
+# sum       total 97
+
+mdd1%>%
+  group_by(lineage, cluster) %>%
+    count(cluster, lineage) %>% group_by(lineage) %>% dplyr::summarise(n=n())
+
+# datatable <- mdd1%>%  group_by(lineage, cluster) %>%  count(cluster, lineage)
+# datatable <- as.matrix(datatable)
+
+table_lineage <- table(mdd1$lineage, mdd1$cluster)
+# png("plots/table_cluster_lineage.png", height=4, width=7, units = "in", res = 72)
+# grid.table(table_lineage)
+# dev.off()
 
 ## Data exploration ####
+## Main question: are any metabolic clusters correlated with infection and/or success? Correct for group sizes
+## If so, are there also correlations with infection site, AMR/virulence genes and/or genetic lineage?
 
 ggplot(mdd1) + 
-  geom_bar(aes(x = cluster, fill = success))
+  geom_bar(aes(x = cluster, fill = country), position = "fill") +
+  scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)")) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+#ggsave("plots/metadata_success.png", width = 10, height = 6)
+# more unsuccessful in non-normal clusters?
 ggplot(mdd1) + 
-  geom_bar(aes(x = cluster, fill = lineage), position = "fill")
+  geom_bar(aes(x = cluster, fill = carriage), position = "fill") +
+  scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)")) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+#ggsave("plots/metadata_carriage.png", width = 10, height = 6)
+# more infection in non-normal clusters?
+ggplot(mdd1) + 
+  geom_bar(aes(x = cluster, fill = site), position = "fill") +
+  scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)")) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+#ggsave("plots/metadata_site.png", width = 10, height = 6)
+# correlation with infection/carriage? focus on that first, then try to minimize categories in site
+ggplot(mdd1) + 
+  geom_bar(aes(x = cluster, fill = pvl), position = "fill") +
+  scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)")) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+#ggsave("plots/metadata_pvl.png", width = 10, height = 6)
+# pvl positive in double and spike, related to infection/carriage?
+ggplot(mdd1) + 
+  geom_bar(aes(x = cluster, fill = lineage), position = "fill") +
+  scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)")) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+#ggsave("plots/metadata_lineage.png", width = 10, height = 6)
+# correlation with CC30, CC22 and CC5?
+
+##Statistical model ####
+# Only keep lineages CC1, CC22, CC30, CC% and CC8
+mdd2 <- mdd1 %>%
+  filter(lineage %in% c("CC1", "CC22", "CC30", "CC5", "CC8"))
+
+#Group cluster types Double, Spike and Post-shoulder together in an Odd cluster type, discard Unclustered ones
+mdd3 <- mdd2 %>%
+  filter(!(cluster %in% c("unclustered")))
+
+mdd3$cluster[mdd3$cluster %in% c("double", "spike", "post_shoulder")] <- "odd" 
+unique(mdd3$cluster)
+
+## Paramatric test: multinomial logistic regression > not enough data for this
+# modeldata <- mlogit.data(mdd2, choice = "cluster", shape = "wide")
+# 
+# # m0 <- mlogit(cluster ~ 1, data = mdd2, reflevel = "normal") #baseline model
+# # summary(m0)
+# m1 <- mlogit(cluster ~ 1 | lineage, data = modeldata, reflevel = "normal")
+# summary(m1)
+# # model with success, carriage, site or pvl does not explain variability in the data
+# # model with lineage does explain variability in the data > attributed to CC30 in cluster wide
+
+## Chi-square test
+
+fisher.test(mdd1$cluster, mdd1$site, simulate.p.value=TRUE) #large dataset, simulated p-value for lineage and site
+#fisher.test(mdd2$cluster, mdd2$lineage, simulate.p.value=TRUE)
+
+# CrossTable(mdd1$lineage, mdd1$cluster)
+# chisq.test(mdd1$lineage, mdd1$cluster)
+# fisher.test(mdd1$cluster, mdd1$lineage, simulate.p.value=TRUE)
+# CrossTable(mdd2$lineage, mdd2$cluster)
+# chisq.test(mdd2$lineage, mdd2$cluster)
+# fisher.test(mdd2$cluster, mdd2$country, simulate.p.value=TRUE)
+
+table(mdd2$lineage, mdd2$cluster)
+table(mdd3$lineage, mdd3$cluster)
+
+#CrossTable(mdd3$cluster, mdd3$lineage)
+#chisq.test(mdd3$cluster, mdd3$success)
+fisher.test(mdd3$cluster, mdd3$success, simulate.p.value=FALSE)
+
+#chisq.test(mdd3$cluster, mdd3$carriage)
+fisher.test(mdd3$cluster, mdd3$carriage, simulate.p.value=FALSE)
+
+chisq.test(mdd3$cluster, mdd3$lineage)
+fisher.test(mdd3$cluster, mdd3$lineage, simulate.p.value=TRUE)
+
+# library(rstatix)
+# fisher_test(table(mdd2$lineage, mdd2$cluster), simulate.p.value=TRUE)
+# pairwise_fisher_test(as.matrix(table(mdd2$lineage, mdd2$cluster)), p.adjust.method = "fdr")
+# how to convert table to two-dimensional contingency table? maybe not working as 2xc comparison, too many categories?
+
+ggplot(mdd3 , aes(cluster, width_peak)) +
+  geom_boxplot(aes(colour = lineage)) +
+  geom_point(aes(colour = lineage)) +
+  facet_wrap(~lineage)
+ggsave("plots/metadata_width_peakCC30.png", width = 10, height = 6)
+
+#Not for further analysis
+# ggplot(mdd1) + 
+#   geom_bar(aes(x = cluster, fill = country), position = "fill") +
+#   scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)"))
+# # biased as mostly Dutch strains included
+# mdd1$year <- as.factor(mdd1$year)
+# ggplot(mdd1) + 
+#   geom_bar(aes(x = cluster, fill = year), position = "fill") +
+#   scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)"))
+# #biased selection + too many variation in year to see effect
+# ggplot(mdd1) + 
+#   geom_bar(aes(x = cluster, fill = mec)) +
+#   scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)"))
+# # no correlation, all MRSA should have mecA (or mecC)
+# ggplot(mdd1) + 
+#   geom_bar(aes(x = cluster, fill = spa_type)) +
+#   scale_x_discrete("Cluster type", label = c("Double (n=5)","Normal (n=56)","Post-shoulder (n=20)","Spike (n=6)","Unclustered (n=2)","Wide (n=8)"))
+# # not enough info to analyse
